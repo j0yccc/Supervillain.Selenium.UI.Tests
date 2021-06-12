91a2d04797a7b1141fb5f3bfc87b6dbab23ff1bd
JoyceChin <joyce.chin@dx.com.au>
2021-06-12 21:58:27 +1000

    Added login page object

--- Supervillain.Selenium.UI.Tests/Supervillain.Selenium.UI.Tests/Pages/Login.cs
+++ Supervillain.Selenium.UI.Tests/Supervillain.Selenium.UI.Tests/Pages/Login.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,14 +8,36 @@ namespace Supervillain.Selenium.UI.Tests.Pages
 {
     class Login : BaseClass
     {
+        //Locator
+        private By usernameLocator = By.XPath("//input[@id='worrior_username']");
+        private By createWorriorLocator = By.Id("warrior");
+        
+        //Page element
+        private IWebElement usernameInput => Driver.FindElement(usernameLocator);
+        private IWebElement createWarriorButton => Driver.FindElement(createWorriorLocator);
+
         protected Login(IWebDriver driver) : base(driver)
         {
+        }
 
+        public void LoginWithUsername(string username)
+        {
+            usernameInput.SendKeys(username);
+
+            createWarriorButton.Click();
         }
       
         protected override bool EvaluateLoadedStatus()
         {
-            throw new NotImplementedException();
+            try
+            {
+                Wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(usernameLocator));
+                return true;
+            }
+            catch
+            {
+                throw new Exception("Create warrior page not load");
+            }    
         }
     }
 }
