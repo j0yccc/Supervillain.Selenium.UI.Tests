98e8d02da3d87217bfb605e95613fdaf3c99567c
JoyceChin <joyce.chin@dx.com.au>
2021-06-12 21:25:48 +1000

    Implementation of initial framework design

---
+++ Supervillain.Selenium.UI.Tests/Supervillain.Selenium.UI.Tests/Pages/BaseClass.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium; 
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Supervillain.Selenium.UI.Tests.Pages
+{
+    public abstract class BaseClass : LoadableComponent<BaseClass>
+    {
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+        private const int DefaultTimeout = 15;
+
+        protected BaseClass(IWebDriver driver)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(driver, new TimeSpan(0, 0, DefaultTimeout));
+        }
+
+        public IWebDriver Driver
+        {
+            get { return _driver; }
+        }
+
+        public WebDriverWait Wait 
+        { 
+            get { return _wait; } 
+        }
+
+
+        public void RefreshPage()
+        {
+            _driver.Navigate().Refresh();
+        }
+
+
+        protected override void ExecuteLoad()
+        {
+        }     
+    }
+}
+
+
---
+++ Supervillain.Selenium.UI.Tests/Supervillain.Selenium.UI.Tests/Pages/Login.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Supervillain.Selenium.UI.Tests.Pages
+{
+    class Login : BaseClass
+    {
+        protected Login(IWebDriver driver) : base(driver)
+        {
+
+        }
+      
+        protected override bool EvaluateLoadedStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
